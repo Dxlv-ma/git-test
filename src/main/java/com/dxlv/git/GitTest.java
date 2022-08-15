4405604ddefce1f632f8d9721d41ee604478c3d9
Dxlv <Dxlv@qq.com>
2022-08-15 20:35:31 +0800

    push commit

--- src/main/java/com/dxlv/git/GitTest.java
+++ src/main/java/com/dxlv/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git!!!!");
         System.out.println("master git!!!!");
         System.out.println("hot-fix git!!!!");
+        System.out.println("push test!!!!");
     }
 }
